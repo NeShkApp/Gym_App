cd75a10cfaeff535181ee055ed7dc7e4ef18431b
Bohdan <bodia.nazar4669@gmail.com>
2023-11-27 20:35:17 +0100

    added the project. Version 1.0.0

---
+++ app/src/main/java/com/example/gym_app/WebActivity.java
@@ -0,0 +1,32 @@
+package com.example.gym_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.artstation.com/bohdan_nazar");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
